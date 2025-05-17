e646fd24c36d583f11acab79edd3bed3f7ebf4f6
HASURO <rentaro170928@icloud.com>
2025-05-17 20:42:48 +0900

    add: storybook

---
+++ src/stories/components/SlideButton.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { fn } from "@storybook/test"
+import { SlideButton } from "../../components/button/SlideButton"
+
+export default {
+  title: "Components/SlideButton",
+  component: SlideButton,
+  args: {
+    totalCount: 5,
+    handleNext: fn(),
+    handleBack: fn(),
+  }
+} satisfies Meta<typeof SlideButton>
+
+export const Default: StoryObj<typeof SlideButton> = {
+  args: {
+    currentNumber: 1,
+  }
+}
+
+export const Disabled: StoryObj<typeof SlideButton> = {
+  args: {
+    isShowIndex: true,
+    currentNumber: 5,
+  }
+}
+
---
+++ src/stories/components/SlideCore.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { SlideCore, SlideFrame } from "../../components/core";
+import { CustomText } from "../../components/text/CustomText";
+
+const slides = [
+  () => <SlideFrame><CustomText size="base" text="hello |world|! I'm |fan||aa" highlightColor="red" /></SlideFrame>,
+  () => <SlideFrame>b</SlideFrame>,
+  () => <SlideFrame>c</SlideFrame>,
+  () => <SlideFrame>d</SlideFrame>,
+];
+
+export default {
+  title: "Components/SlideCore",
+  component: SlideCore,
+  args: {
+    slides: slides
+  }
+} satisfies Meta<typeof SlideCore>
+
+export const Default: StoryObj<typeof SlideCore> = {
+  args: {
+  }
+}
